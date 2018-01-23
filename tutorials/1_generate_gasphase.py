0c3569c139bd190c18bf7b275a45cb669f8d9b58
mhangaard <mhangaard@gmail.com>
2018-01-23 14:04:03 -0800

    migrate tutorial

---
+++ tutorials/1_generate_gasphase.py
@@ -0,0 +1,10 @@
+from catkit.pathways import ReactionNetwork
+
+db_name = 'example-C2H6.db'
+
+with ReactionNetwork(db_name=db_name) as rn:
+
+    molecules = rn.molecule_search(
+        element_pool={'C': 2, 'H': 6},
+        multiple_bond_search=False)
+    rn.save_molecules(molecules)
