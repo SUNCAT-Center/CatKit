bb589974ad3b7aa48abee907d352b6ec0a33810b
Jacob Boes <jacobboes@gmail.com>
2018-01-31 09:30:10 -0800

    version 0.1.0 / first testing

--- setup.py
+++ setup.py
@@ -3,11 +3,22 @@ from setuptools import setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+with open('readme.org') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='CatKit',
-    version='0.0.1.dev0',
+    version='0.1.0',
     packages=['catkit'],
-    license='GNU General Public License v3.0',
-    long_description=open('readme.org').read(),
+    author='Jacob Boes',
+    author_email='jrboes@stanford.edu',
+    license=license,
+    description='Catalysis Automation Tool Kit',
+    long_description=readme,
+    data_files=['requirements.txt', 'LICENSE'],
+    setup_requires=['nose>=1.0'],
     install_requires=required,
 )
