61410714f7b47fb811e0666260c59957d833660a
Kirsten Winther <winther@stanford.edu>
2018-05-18 10:40:06 -0700

    entry_points for cathub cli

--- setup.py
+++ setup.py
@@ -26,6 +26,10 @@ setuptools.setup(
         'catflow',
         'cathub'
     ],
+    entry_points='''
+         [console_scripts]
+         cathub=cathub:cli
+      ''',
 
     install_requires=required,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
