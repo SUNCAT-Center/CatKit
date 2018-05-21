5843034accf59a9f7108ae1a0700547d5a241c83
Kirsten Winther <winther@stanford.edu>
2018-05-20 21:52:00 -0700

    get structures from server

--- cathub/query.py
+++ cathub/query.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+import os
+import json
 import requests
 import pprint
+import ase.db
+
 from cathub.cathubsqlite import CathubSQLite
 
 all_columns = {'reactions': ['chemicalComposition', 'surfaceComposition',
@@ -89,7 +93,7 @@ def graphql_query(table='reactions',
     return statement
 
 
-def get_reactions(n_results=20, write_local=False, **kwargs):
+def get_reactions(n_results=20, write_db=False, **kwargs):
     queries = {}
     for key, value in kwargs.items():
         key = map_column_names(key)
@@ -110,15 +114,18 @@ def get_reactions(n_results=20, write_local=False, **kwargs):
     data = query(table='reactions', subtables=subtables,
                  columns=all_columns['reactions'],
                  n_results=n_results, queries=queries)
-    import json
-    with CathubSQLite('Reaction.db') as db:
-        for row in data['data']['reactions']['edges']:
+
+    if not write_db:
+        return data
+
+    for row in data['data']['reactions']['edges']:
+        with CathubSQLite('Reactions.db') as db:       
             row = row['node']
             key_values = {}
             for key in all_columns['reactions']:
                 v = row[key]
-                if isinstance(v, unicode):
-                    v = v.encode('utf-8')
+                #if isinstance(v, unicode):
+                #    v = v.encode('utf-8')
                 try:
                     v = json.loads(v)
                 except:
@@ -138,18 +145,29 @@ def get_reactions(n_results=20, write_local=False, **kwargs):
                 energy_corrections = None
             key_values['ase_ids'] = ase_ids
             key_values['energy_corrections'] = ase_ids
-            
-            id = db.check(key_values['chemical_composition'], key_values['reaction_energy'])
-            if id is None:
-                id = db.write(key_values)
-            else:
-                db.update(id, key_values)
 
+            # publications
             pub_key_values = {}
             row_p = row['publication']
             for key in all_columns['publications']:
                 pub_key_values[convert(key)] = row_p[key]
             db.write_publication(pub_key_values)
+            
+            # reactions and reaction_systems
+            id = db.check(key_values['chemical_composition'], key_values['reaction_energy'])
+            if id is None:
+                id = db.write(key_values)
+            else:
+                db.update(id, key_values)
+        if ase_ids is not None:
+            # Ase structures
+            with ase.db.connect('Reactions.db') as ase_db:
+                for unique_id in ase_ids.values():
+                    if ase_db.count('unique_id={}'.format(unique_id)) == 0:
+                        atomsrow = get_atomsrow_by_id(unique_id)                        
+                        ase_db.write(atomsrow) 
+
+    return data
     
 def get_publications(**kwargs):
     queries = {}
@@ -168,14 +186,25 @@ def get_publications(**kwargs):
     return query(table='publications', columns=publication_columns,
                   queries=queries)
 
-def get_structure_by_id(unique_id):
-    db = ase.db.connect('postgresql://catvisitor:$DB_PASSWORD@catalysishub.c8gwuc8jwb7l.us-west-2.rds.amazonaws.com:5432/catalysishub')
-    
+
+def get_ase_db():
+     ps = os.environ.get('DB_PASSWORD')
+     return  ase.db.connect('postgresql://catvisitor:{}@catalysishub.c8gwuc8jwb7l.us-west-2.rds.amazonaws.com:5432/catalysishub'.format(ps))
+
+
+def get_atomsrow_by_id(unique_id):   
+    db = get_ase_db()    
     row = db.get('unique_id={}'.format(unique_id))    
     return row
 
+#def get_atomsrow_by_ids(unique_ids):
+#    db = get_ase_db()
+#    con = db._connect()
+#    cur = con.cursor()
+    
+
 def get_atoms_by_id(unique_id):
-    row = get_structure_by_id(unique_id)
+    row = get_atomsrow_by_id(unique_id)
     return row.toatoms()
 
 def map_column_names(column):
