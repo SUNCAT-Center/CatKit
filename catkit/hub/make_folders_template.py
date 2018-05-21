82f144db04dbee98df09de066f06141c662fd657
Kirsten Winther <winther@stanford.edu>
2018-05-21 16:41:21 -0700

    pycodestyle

--- catkit/hub/make_folders_template.py
+++ catkit/hub/make_folders_template.py
@@ -4,54 +4,38 @@ import os
 import sys
 import json
 
-if 'SHERLOCK' in os.environ:  # Sherlock 1 or 2
-    sherlock = os.environ['SHERLOCK']
-    if sherlock == '1':
-        catbase = '/home/winther/data_catapp'
-    elif sherlock == '2':
-        catbase = '/home/users/winther/data_catapp'
-elif 'SLAC_ENVIRON' in os.environ:  #SUNCAT
-    catbase = '/nfs/slac/g/suncatfs/data_catapp/'
-else:
-    catbase = None
-
-sys.path.append(catbase)
-
 import catkit.hub.ase_tools
 from catkit.hub.tools import extract_atoms, check_reaction
 
 username = os.environ['USER']
 
+
 # ---------publication info------------
 def main(
-    title='Fancy title',  # work title if not yet published
-    authors=['Doe, John', 'Einstein, Albert'],  # name required
-    journal='',
-    volume='',
-    number='',
-    pages='',
-    year='2017',  # year required
-    publisher='',
-    doi='',
-    tags=[],
-    DFT_code='Quantum ESPRESSO',  # for example 'Quantum ESPRESSO'
-    DFT_functional='BEEF-vdW',  # For example 'BEEF-vdW'
-
-    #  ---------reaction info-----------
-
-    reactions=[
-        {'reactants': ['2.0H2Ogas', '-1.5H2gas', 'star'],
-         'products': ['OOHstar@ontop']},
-        #{'reactants': ['CCH3'], 'products': ['C', 'CH3']},
-        #{'reactants': ['CH3star'], 'products': ['CH3gas', 'star']}
-    ],
-    energy_corrections= {}, # For example: {'H2gas': 0.1},
-    bulk_compositions=['Pt', 'Ag'],
-    crystal_structures=['fcc', 'hcp'],
-    facets=['111'],
-    custom_base=None,
-    ):
-
+        title='Fancy title',  # work title if not yet published
+        authors=['Doe, John', 'Einstein, Albert'],  # name required
+        journal='',
+        volume='',
+        number='',
+        pages='',
+        year='2017',  # year required
+        publisher='',
+        doi='',
+        tags=[],
+        DFT_code='Quantum ESPRESSO',  # for example 'Quantum ESPRESSO'
+        DFT_functional='BEEF-vdW',  # For example 'BEEF-vdW'
+        #  ---------reaction info-----------
+        reactions=[
+            {'reactants': ['2.0H2Ogas', '-1.5H2gas', 'star'],
+             'products': ['OOHstar@ontop']},
+            # {'reactants': ['CCH3'], 'products': ['C', 'CH3']},
+            # {'reactants': ['CH3star'], 'products': ['CH3gas', 'star']}
+        ],
+        energy_corrections={},  # For example: {'H2gas': 0.1},
+        bulk_compositions=['Pt', 'Ag'],
+        crystal_structures=['fcc', 'hcp'],
+        facets=['111'],
+        custom_base=None):
     """
     Dear all
 
@@ -61,8 +45,8 @@ def main(
     and assign the right information to the arguments in the function above.
 
     You can change the parameters and run the script several times if you,
-    for example, are using different functionals or are doing different reactions
-    on different surfaces.
+    for example, are using different functionals or are doing different
+    reactions on different surfaces.
 
     Include the phase if necessary:
 
@@ -81,37 +65,41 @@ def main(
     For example, we can write the adsorption of CH2 as:
           CH4(g) - H2(g) + * -> CH2*
 
-    Here you would have to write ['CH4gas', 'H2gas', 'star'] as 'reactants' entry.
+    Here you would have to write ['CH4gas', 'H2gas', 'star'] as 'reactants'
+    entry.
 
     The reaction argument is a list of dictionaries. See examples:
 
     reactions = [
-        {'reactants': ['CH4gas', '-H2gas', 'star'], 'products': ['CH2star@bridge']},
-        {'reactants': ['CH4gas', '-0.5H2gas', 'star'], 'products': ['CH3star@top']}
+        {'reactants': ['CH4gas', '-H2gas', 'star'],
+         'products': ['CH2star@bridge']},
+        {'reactants': ['CH4gas', '-0.5H2gas', 'star'],
+         'products': ['CH3star@top']}
         ]
 
-    A new dictionary is required for each reaction, and should include two lists,
-    'reactants' and 'products'. Remember to include a minus sign and prefactor in
-    the name when relevant. If your reaction is not balanced, you will
-    receive an error when running the script.
+    A new dictionary is required for each reaction, and should include two
+    lists, 'reactants' and 'products'. Remember to include a minus sign and
+    prefactor in the name when relevant. If your reaction is not balanced,
+    you will receive an error when running the script.
 
     # ---------------surface info---------------------
 
     facets # If complicated structure: use term you would use in publication
     """
 
-    #  ----------- You're done!------------------------
-
     # Check reactions
-    #assert len(reactants) == len(products_A) == len(products_B)
+    assert len(reactants) == len(products_A) == len(products_B)
 
     for reaction in reactions:
         check_reaction(reaction['reactants'], reaction['products'])
 
     # Set up directories
-    base = '%s/%s/' % (catbase, username)
+
     if custom_base is not None:
         base = custom_base + '/'
+    else:
+        catbase = os.path.abspath(os.path.curdir)
+        base = '%s/%s/' % (catbase, username)
 
     if not os.path.exists(base):
         os.mkdir(base)
@@ -144,7 +132,6 @@ def main(
         energy_txt = publication_base + 'energy_corrections.txt'
         json.dump(energy_corrections, open(energy_txt, 'wb'))
 
-
     def create(path):
         if not os.path.exists(path):
             os.mkdir(path)
@@ -164,7 +151,6 @@ def main(
         gas_names += [clear_state(clear_prefactor(rnames[i]))
                       for i in range(len(states)) if states[i] == 'gas']
 
-
     for name in set(gas_names):
         with open(gas_base + 'MISSING: {}_gas.traj'.format(name), 'w'):
             pass
@@ -173,12 +159,14 @@ def main(
         for crystal_structure in crystal_structures:
             bulk_name = bulk + '_' + crystal_structure
             facet_base = create(bulk_base + bulk_name + '/')
-            with open(facet_base + 'MISSING: {}_bulk.traj'.format(bulk_name), 'w'):
+            with open(facet_base + 'MISSING: {}_bulk.traj'.format(bulk_name),
+                      'w'):
                 pass
 
             for facet in facets:
                 reaction_base = create(facet_base + facet + '/')
-                with open(reaction_base + 'MISSING: empty_slab.traj'.format(bulk_name), 'w'):
+                with open(reaction_base + 'MISSING: empty_slab.traj'
+                          .format(bulk_name), 'w'):
                     pass
 
                 for i in range(len(reactions)):
@@ -186,18 +174,21 @@ def main(
                     pname = '_'.join(reactions[i]['products'])
                     reaction_name = '__'.join([rname, pname])
                     base = create(reaction_base + reaction_name + '/')
-                    rnames = [r.split('@')[0] for r in reactions[i]['reactants'] +
+                    rnames = [r.split('@')[0] for r in
+                              reactions[i]['reactants'] +
                               reactions[i]['products']]
                     states = [get_state(r) for r in rnames]
 
                     ads_names = [clear_prefactor(clear_state(rnames[i]))
-                                 for i in range(len(states)) if states[i] == 'star']
+                                 for i in range(len(states))
+                                 if states[i] == 'star']
 
                     for ads in ads_names:
                         if ads == '':
                             continue
                         with open(base + 'MISSING: {}.traj'.format(ads), 'w'):
                             pass
+
 
 if __name__ == "__main__":
     main()
