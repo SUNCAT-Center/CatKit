43a7a23346a0cb1da407a31bfe4fee7e3df07128
Kirsten Winther <winther@stanford.edu>
2018-05-22 14:47:50 -0700

    new path for cathub cli

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     package_dir={'catkit': 'catkit'},
     entry_points='''
          [console_scripts]
-         cathub=catkit.hub:cli
+         cathub=catkit.hub.cli:cli
       ''',
     install_requires=requirements,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
