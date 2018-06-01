c70e756f972ea168b56fe0b7df0842aea861f566
Jacob Boes <jacobboes@gmail.com>
2018-06-01 06:10:05 -0700

    more tests for slab generation

--- catkit/gen/tests/test_surfaces.py
+++ catkit/gen/tests/test_surfaces.py
@@ -1,6 +1,7 @@
 import unittest
 from catkit.gen.surface import SlabGenerator
 from ase.build import bulk
+import numpy as np
 
 
 class TestSlabGenerator(unittest.TestCase):
@@ -8,11 +9,12 @@ class TestSlabGenerator(unittest.TestCase):
 
     def test_slab_generator(self):
         """Test that no exception is raised when running SlabGenerator."""
-        atoms = bulk('Pd', 'fcc', a=4, cubic=True)
+        atoms = bulk('Pd', 'fcc', cubic=True)
 
         SlabGenerator(atoms, miller_index=(1, 1, 1), layers=6, vacuum=4)
         SlabGenerator(atoms, miller_index=(1, 1, 0), layers=6, vacuum=4)
         SlabGenerator(atoms, miller_index=(1, 0, 0), layers=6, vacuum=4)
+        SlabGenerator(atoms, miller_index=(0, 0, 0, 1), layers=6)
 
     def test_terminations(self):
         """Test termination finding on fcc(211)."""
@@ -23,6 +25,29 @@ class TestSlabGenerator(unittest.TestCase):
             atoms, miller_index=(2, 1, 1), layers=8, vacuum=4)
 
         gen.get_slab(iterm=1)
+
+    def test_gcd_miller_index(self):
+        """Test that miller indices are properly reduces to their
+        greatest common divisor.
+        """
+        atoms = bulk('Pd', 'fcc')
+        gen = SlabGenerator(atoms, miller_index=(3, 3, 3), layers=3)
+
+        np.testing.assert_array_equal(gen.miller_index, [1, 1, 1])
+
+    def test_no_standardization(self):
+        """Test that the slab generator functions if not standardized."""
+        atoms = bulk('Pd', 'fcc', cubic=True)
+        SlabGenerator(atoms, miller_index=(1, 1, 1),
+                      layers=3, standardize_bulk=False)
+
+    def test_matrix_notation_search(self):
+        """Test the matrix notation algorithm."""
+        atoms = bulk('Pd', 'fcc', cubic=True)
+        gen = SlabGenerator(atoms, miller_index=(1, 1, 1), layers=3)
+
+        gen.get_slab(size=1)
+        gen.get_slab(size=[[1, 0], [0, 1]])
 
 
 if __name__ == '__main__':
