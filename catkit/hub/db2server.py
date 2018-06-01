823d419d2a6b0aec3b0e4a17910a703ef06c3afa
Kirsten Winther <winther@stanford.edu>
2018-06-01 15:07:32 -0700

    start_id not used anymore

--- catkit/hub/db2server.py
+++ catkit/hub/db2server.py
@@ -2,7 +2,7 @@ from .postgresql import CathubPostgreSQL
 from sys import argv, stdout
 
 
-def main(dbfile, start_id=1, write_reaction=True, write_ase=True,
+def main(dbfile, write_reaction=True, write_ase=True,
          write_publication=True, write_reaction_system=True,
          block_size=1000, start_block=0,
          db_user='catroot',
@@ -13,7 +13,7 @@ def main(dbfile, start_id=1, write_reaction=True, write_ase=True,
 
     db = CathubPostgreSQL(user=db_user, password=db_password)
     stdout.write('Established SQL Server connection.\n')
-    db.transfer(dbfile, start_id=start_id, write_reaction=write_reaction,
+    db.transfer(dbfile, write_reaction=write_reaction,
                 write_ase=write_ase,
                 write_publication=write_publication,
                 write_reaction_system=write_reaction_system,
