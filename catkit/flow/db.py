505e520ae8b705be1caaeee964d9521065d46904
Jacob Boes <jacobboes@gmail.com>
2018-10-01 16:14:09 -0700

    update catflow with connection module

--- catkit/flow/db.py
+++ catkit/flow/db.py
@@ -6,6 +6,74 @@ import json
 Base = declarative_base()
 
 
+class Connect():
+    """A class for accessing a temporary SQLite database. This
+    function works as a context manager and should be used as follows:
+
+    with Connect() as db:
+        (Perform operation here)
+    """
+
+    def __init__(self, engine):
+        """Initialize the database"""
+        self.engine = sqa.create_engine(engine)
+        self.session = sqa.orm.sessionmaker(bind=self.engine)
+
+    def __enter__(self):
+        """This function is automatically called whenever the class
+        is used together with a 'with' statement.
+        """
+        Base.metadata.create_all(self.engine)
+        self.cursor = self.session()
+
+        return self
+
+    def __exit__(self, type, value, tb):
+        """Upon exiting the 'with' statement, __exit__ is called."""
+        self.cursor.commit()
+        self.cursor.close()
+
+    def add_bulk_entry(self, images, calculator):
+        """Add a bulk relaxation trajectory to the database. This requires
+        the calculator parameters, the calculator being used, and the
+        images from the trajectory.
+
+        The info of the initial and finals atoms object are parsed
+        for a prototype_id.
+
+        Parameters
+        ----------
+        images : list of Atoms objects
+            Relaxation trajectory for a bulk calculation given in
+            sequential order.
+        calculator : Calculator object
+            ASE | decaf-espresso calculator object used for the
+            relaxation trajectory.
+        """
+        parameters = images[0].info['calculator_parameters']
+
+        calculator = Calculator(
+            calculator.name,
+            parameters)
+        self.cursor.add(calculator)
+
+        structure = Structure(images[0], calculator=calculator)
+        self.cursor.add(structure)
+
+        # We will need to store the final and initial prototypes
+        init_prototype_id = images[0].info['prototype_id']
+#         init_prototype = Bulk(prototype_id, structure=structure)
+
+        for atoms in images[1:]:
+            structure = Structure(
+                atoms, calculator=calculator, parent=structure)
+            self.cursor.add(structure)
+
+        prototype_id = atoms.info['prototype_id']
+        bulk_prototype = Bulk(prototype_id, structure=structure)
+        self.cursor.add(bulk_prototype)
+
+
 class Calculator(Base):
     __tablename__ = 'calculator'
     id = sqa.Column(sqa.Integer, primary_key=True)
@@ -56,6 +124,7 @@ class Structure(Base):
         if any(atoms.get_tags()):
             self.tags = atoms.get_tags().tolist()
 
+        # Read the constraints into an array
         self.constraints = None
         if atoms.constraints:
             self.constraints = np.ones((len(atoms), 3), dtype=int)
@@ -70,6 +139,7 @@ class Structure(Base):
         parameters = [
             'energy', 'forces', 'stress', 'charges', 'magmom', 'magmoms']
 
+        # Read results
         if atoms._calc:
             for parameter in parameters:
                 result = atoms._calc.results.get(parameter)
