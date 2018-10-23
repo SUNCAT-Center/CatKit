ddf632327e01596d08d6593a1ffdc194cc673b72
Kirsten Winther <winther@stanford.edu>
2018-10-23 11:21:15 -0700

    cleanup - no git requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@ import setuptools
 
 with open('requirements.txt', 'r') as f:
     requirements = f.readlines()
-    git_requirements = [f.strip('\n') for f in requirements if 'git' in f]
     requirements = [f for f in requirements if 'git' not in f]
-    requirements += ['ase>=3.16.2']
 
 with open('readme.org', 'r') as f:
     readme = f.read()
@@ -30,13 +28,8 @@ setuptools.setup(
         'catkit.flow',
     ],
     package_dir={'catkit': 'catkit'},
-    #entry_points='''
-    #     [console_scripts]
-    #     cathub=catkit.hub.cli:cli
-    #  ''',
     install_requires=requirements,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
-    dependency_links=git_requirements,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
