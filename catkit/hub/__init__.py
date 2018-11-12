5eac3a9524f8f0ce69356c288189ab1e6c4baf3b
Jacob Boes <jacobboes@gmail.com>
2018-11-12 11:09:03 -0800

    Change exception to warning

--- catkit/hub/__init__.py
+++ catkit/hub/__init__.py
@@ -1,1 +1,2 @@
-raise DeprecationWarning("The cathub module will be moved from catkit.hub.* to cathub.* in a future release.")
+import warnings
+warnings.warn("The cathub module will be moved from catkit.hub.* to cathub.* in a future release.", DeprecationWarning)
