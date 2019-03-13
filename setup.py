01fe94aa2e735ea243a8d251660b240b4639731d
Jacob Boes <jacobboes@gmail.com>
2019-03-12 20:17:43 -0700

    modifications to setup

--- setup.py
+++ setup.py
@@ -45,5 +45,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
