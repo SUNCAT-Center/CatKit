cb96d8a34f08d9684eb35682fb23790f118dc869
Kirsten Winther <winther@stanford.edu>
2019-11-15 11:56:35 -0800

    get rid of Python 2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
     ],
     package_dir={'catkit': 'catkit'},
     install_requires=requirements,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    python_requires='>=3.5, <4',
 
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -36,9 +36,6 @@ setuptools.setup(
         'Topic :: Scientific/Engineering :: Chemistry',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
